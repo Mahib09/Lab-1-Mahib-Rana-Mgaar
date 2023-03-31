d5ce54f972f255859c8f4c1e4305ea3fa49153f5
Mahib09 <Mahib.Ranamagar@triosstudent.com>
2023-03-31 15:27:29 -0400

    Update 1.py

--- Lab 1/1.py
+++ Lab 1/1.py
@@ -1,9 +1,12 @@
+#predefined variables
 cleaningrate=60
 cavityrate=200
 xrayrate=100
 tax=15/100
 discount1=5/100
 discount2=10/100
+
+#inputs
 patient_name=str(input('Enter your Name:'))
 cleaning=str(input('Was cleaning performed? (y/n):'))
 if cleaning=="y":
@@ -20,6 +23,8 @@ if xray=="y":
     total=total+xrayrate
 else:
     total=total
+
+#calculations
 totalwithtax= (total + (total*tax))
 if totalwithtax > 300:
     billtotal=(totalwithtax-(totalwithtax*discount2))
@@ -27,10 +32,12 @@ elif totalwithtax > 200:
     billtotal=(totalwithtax-(totalwithtax*discount1))
 else:
     billtotal=totalwithtax
+
+#output
 print('         Melanie Dental Clinic           ')
 print('    * ----------------------------*     ')
 print('         Receipt for patient name:')
-print('                ',patient_name)
+print('               ',patient_name)
 print('             subtotal:', total )
 print('             tax:', totalwithtax-total)
 print('                ',billtotal)
